baa57f9a1693acc09e757c5ab6bd58a6bd580be3
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-17 16:13:47 -0500

    install npm packages and setup initial file structure and server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 8 // sets cookie to expire after 8 hours to require logging back in
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
