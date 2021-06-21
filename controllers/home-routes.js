814892743967de60fed96e577906f4220f75f758
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-06-21 00:31:04 -0500

    created functions for comment and add, edit, and delete posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,11 +3,11 @@ const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'title', 'content', 'create_at'],
+    attributes: ['id', 'title', 'content', 'created_at'],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'create_at'],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
           attributes: ['username']
